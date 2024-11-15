f24627877795fc2060b887503cdb75f67f9706be
dusmcd <dustinmcdowell1989@gmail.com>
2024-11-15 13:55:43 -0500

    Creating and viewing students

---
+++ MusicLessonSch/Controllers/StudentsController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MusicLessonSch.Data;
+using MusicLessonSch.Models;
+
+namespace MusicLessonSch.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly MusicLessonSchContext _context;
+
+        public StudentsController(MusicLessonSchContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var students = await _context.Student.Include(s => s.Instruments).ToListAsync();
+            return View(students);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var instruments = await _context.Instrument.ToListAsync();
+            List<InstrumentViewModel> instrumentsVM = new List<InstrumentViewModel>();
+            foreach(var item in instruments)
+            {
+                var instrumentVM = new InstrumentViewModel
+                {
+                    Id = item.Id,
+                    Name = item.Name,
+                };
+                instrumentsVM.Add(instrumentVM);
+            }
+            var student = new StudentViewModel
+            {
+                Instruments = instrumentsVM,
+            };
+            return View(student);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id","Name","PhoneNumber","Email","Age", "InstrumentId")] StudentViewModel studentVM)
+        {
+            var student = new Student
+            {
+                Name = studentVM.Name,
+                PhoneNumber = studentVM.PhoneNumber,
+                Email = studentVM.Email,
+                Age = studentVM.Age,
+            };
+            var instrument = _context.Instrument.Find(studentVM.InstrumentId);
+            student.Instruments.Add(instrument!);
+            _context.Student.Add(student);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MusicLessonSch/Models/StudentViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicLessonSch.Models
+{
+    public class StudentViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required, DisplayName("Phone Number")]
+        public string? PhoneNumber { get; set; }
+
+        public string? Email { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+
+        public List<Teacher> Teachers { get; } = [];
+
+        public List<InstrumentViewModel> Instruments { get; set; } = [];
+
+        [Required, DisplayName("Instrument")]
+        public int InstrumentId { get; set; }
+
+        [DisplayName("Instrument Name")]
+        public string? InstrumentName { get; set; }
+    }
+}
